740c4b4b8f8c8d304acbb50dbdda7de17507c956
dyan017 <dyan017@hotmail.com>
2014-10-27 10:47:23 +1300

    New Project
    Setup for Robo test

---
+++ Apollo/Phone/src/androidTest/java/genesis/apollo/RoboTest.java
@@ -0,0 +1,19 @@
+package genesis.apollo;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+/**
+ * Created by lyang on 25/10/2014.
+ */
+@RunWith(RobolectricTestRunner.class)
+public class RoboTest {
+   @Test
+   public void testTrueIsTrue() throws Exception {
+            Assert.assertEquals(true, true);
+        }
+}
+
